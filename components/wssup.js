435495e2f69e41be664ff5c809afeefbcba29166
aminamir97 <37938147+aminamir97@users.noreply.github.com>
2022-05-22 21:10:43 +0300

    fully 1

--- components/wssup.js
+++ components/wssup.js
@@ -6,7 +6,7 @@ import wsicon from "../assets/wsicon.png";
 export const Wssup = () => {
   return (
     <div className="wssuparea">
-      <Link href={"https://wa.me/905523943639"} >
+      <Link href={"https://wa.me/905523943639?text=Hello%20mr.Amin%20from%20portfolio"} >
         <Image src={wsicon.src} width={50} height={50} />
 
 
